9de5ea50ebd26d036a16f331c19eb39c205e8ece
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-21 13:21:39 -0400

    cta done

--- src/Components/CtaList.jsx
+++ src/Components/CtaList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const CtaList = ({ item }) => {
-  return <div className="cta__list__items">{item.id}</div>;
+  return (
+    <div className="cta__list__items">
+      <img className="cta__list__items-icon" src={item.icon} alt="" />
+      <h4 className="cta__list__items-heading">{item.heading}</h4>
+      <p className="cta__list__items-paragraph">{item.paragraph}</p>
+    </div>
+  );
 };
 
 export default CtaList;

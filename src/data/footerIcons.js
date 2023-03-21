ea637ab02a92c1302b3d936b52bdbf78b93fd997
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-21 18:32:30 -0400

    footer left done

---
+++ src/data/footerIcons.js
@@ -0,0 +1,22 @@
+export const footerIcons = [
+  {
+    id: 1,
+    icon: "assets/images/icon-facebook.svg",
+  },
+  {
+    id: 2,
+    icon: "/assets/images/icon-youtube.svg",
+  },
+  {
+    id: 3,
+    icon: "/assets/images/icon-twitter.svg",
+  },
+  {
+    id: 4,
+    icon: "/assets/images/icon-pinterest.svg",
+  },
+  {
+    id: 5,
+    icon: "/assets/images/icon-instagram.svg",
+  },
+];

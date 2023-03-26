67aa70f10addbb5d66d57ac8b062c4b5a6662c58
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-26 01:18:54 -0400

    dashboard part 1

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,10 +2,26 @@ import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth/AuthContext";
 import newRequest from "../utils/newRequest";
 import AccountList from "./AccountList";
+import {
+  BiBarChartSquare,
+  BiMessageSquareAdd,
+  BiArea,
+  BiMoney,
+} from "react-icons/bi";
+import {
+  BsCreditCard2Front,
+  BsCalendarEvent,
+  BsChatLeftDots,
+  BsSendCheck,
+} from "react-icons/bs";
+import { MdOutlineSettingsApplications } from "react-icons/md";
 
 const Account = () => {
   const [userAccountNumbers, setUserAccountNumbers] = useState([]);
   const { user } = useContext(AuthContext);
+  const [state] = useState({
+    logo: "assets/images/logo.svg",
+  });
 
   useEffect(() => {
     const userAccounts = async () => {
@@ -24,14 +40,100 @@ const Account = () => {
   }, []);
 
   return (
-    <div>
-      {userAccountNumbers
-        ? userAccountNumbers.map((item) => {
-            return <AccountList item={item} key={item.id} />;
-          })
-        : null}
+    <div className="account__container">
+      <div className="account__wrapper">
+        <div className="account">
+          <div className="account__left">
+            <div className="account__left__content">
+              <img src={state.logo} alt="" />
+              <ul>
+                <li>
+                  <span>
+                    <BiBarChartSquare />
+                  </span>
+                  Dashboad
+                </li>
+                <li>
+                  <span>
+                    <BsSendCheck />
+                  </span>
+                  Transactions
+                </li>
+                <li>
+                  <span>
+                    <BsCreditCard2Front />
+                  </span>
+                  Accounts
+                </li>
+                <li>
+                  <span>
+                    <MdOutlineSettingsApplications />
+                  </span>
+                  Settings
+                </li>
+                <li>
+                  <span>
+                    <BsCalendarEvent />
+                  </span>
+                  Calendar
+                </li>
+                <li>
+                  <span>
+                    <BsChatLeftDots />
+                  </span>
+                  Chat
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="account__middle">
+            <div className="account__middle__header">
+              <h2 className="account__middle__header-heading">Dashboard</h2>
+              <h4 className="account__middle__header-time">
+                <span> showing for</span> : {new Date().toLocaleString()}
+              </h4>
+            </div>
+            <div className="account__middle__top">
+              <ul>
+                <li>
+                  <span>
+                    <BsSendCheck />
+                  </span>
+                  send money
+                </li>
+                <li>
+                  <span>
+                    <BiMessageSquareAdd />
+                  </span>
+                  deposit money
+                </li>
+                <li>
+                  <span>
+                    <BiArea />
+                  </span>
+                  request withdrawal
+                </li>
+
+                <li>
+                  <span>
+                    <BiMoney />
+                  </span>
+                  view balance
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="account__right">right</div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Account;
+
+// {userAccountNumbers
+//   ? userAccountNumbers.map((item) => {
+//       return <AccountList item={item} key={item.id} />;
+//     })
+//   : null}

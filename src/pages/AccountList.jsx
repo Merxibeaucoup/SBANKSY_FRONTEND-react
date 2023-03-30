91cb4e047a34355efeae0907688cf42a51b8c86c
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-29 20:01:50 -0400

    modals started

--- src/pages/AccountList.jsx
+++ src/pages/AccountList.jsx
@@ -5,9 +5,17 @@ const AccountList = ({ item }) => {
     <div>
       <ul>
         <li>
-          <p>
-            {item.number} : {item.balance}
-          </p>
+          <div className="money">
+            <div className="money__number">
+              <h2>Nickname</h2>
+              <h5>{item.number}</h5>
+            </div>
+            <div className="money__balance">
+              <h5>
+                balance: <span>${item.balance}</span>{" "}
+              </h5>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
